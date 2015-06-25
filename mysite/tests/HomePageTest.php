486fd259de20013413a950ff8149d0f4c2f798fc
Darren Inwood <darren@stripetheweb.com>
2015-06-25 13:07:41 +1200

    Added test for homepage carousel

---
+++ mysite/tests/HomePageTest.php
@@ -0,0 +1,27 @@
+<?php
+
+class HomePageTest extends SapphireTest {
+	
+	// Make sure that URL home/carousel returns JSON containing
+	// three items.
+	// {
+	//    "items" : [
+	//       {
+	//         "title" : "asdf",
+	//         "image" : "assets/Uploads/asdf.png",
+	//         "caption" : "adsf asdf asdf"
+	//       },
+	//       ...
+	//    ]
+	// }
+	public function testHomepageCarousel() {
+		$html = Director::test('home/carousel');
+
+		$data = json_decode($html->getBody(), true);
+
+		$this->assertNotNull($data, 'Carousel URL home/carousel does not return valid JSON');
+		$this->assertArrayHasKey('items', $data, 'Carousel JSON does not contain items');
+		$this->assertCount(3, $data['items'], 'Carousel JSON does not contain three items');
+	}
+
+}
